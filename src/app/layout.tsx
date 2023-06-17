0ef5383b19517c2088e0765becfdc205a17e52af
Eleanor Sim <eleanorsimjoann@gmail.com>
2023-06-17 11:23:19 +0000

    feat: add basic nextauth authentication with auto sign in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import AuthProvider from '../lib/auth/auth-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   )
 }
