0185e80483f127d9e353cda8c1b5d64b4f62b9f6
Eleanor Sim <eleanorsimjoann@gmail.com>
2023-08-06 15:10:44 +0000

    refactor: pull stuff out into methods

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,15 @@
 import { randomUUID } from 'crypto';
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth, { Account, NextAuthOptions } from 'next-auth';
 import KeycloakProvider from "next-auth/providers/keycloak";
 import { TOKEN_STATE, TOKEN_TYPES } from '@/src/lib/constants';
 import { cacheToken } from '@/src/lib/tokenCache';
 import refreshAccessToken from '@/src/lib/refreshAccessToken';
 import { getTokenExpiryFromCache } from '../../../../lib/tokenCache';
+import { JWT } from 'next-auth/jwt';
+
+const ONE_MINUTE = 60 * 1000
+const ZERO_MINUTES = 0 * ONE_MINUTE;
+const TEN_MINUTES = 10 * ONE_MINUTE;
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -16,55 +21,25 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async session({session, token}) {
+      // Augment session with refresh token details so client can handle expiry
       session.refresh_token_expiry = token.refresh_token_expiry,
       session.refresh_token_state = token.refresh_token_state
       return session
     },
     async jwt({ token, trigger, account }) {
       if (trigger === "signIn") {
-        // assign ID to identify session
-        const sessionId = randomUUID()
-        token.id = sessionId
-        token.refresh_token_expiry = account?.refresh_expires_in ?? 0
-        // extract tokens
-        const access_token: JWTWithExpiry = {
-          token: account?.access_token ?? "",
-          expires_in: (account?.expires_at ?? Date.now()) - Math.floor(Date.now() / 1000),
-          type: TOKEN_TYPES.ACCESS_TOKEN
-        }
-
-        const refresh_token: JWTWithExpiry = {
-          token: account?.refresh_token ?? "",
-          expires_in: account?.refresh_expires_in ?? 0,
-          type: TOKEN_TYPES.REFRESH_TOKEN
-        }
-
-        // cache these into redis
-        await cacheToken(access_token, sessionId)
-        await cacheToken(refresh_token, sessionId)
+        handleTokensOnSignIn(token, account)
         return token
       }
       
-      // go to redis to get both refresh & access token
+      // Get token expiries from cache
       const accessTokenExpiry = await getTokenExpiryFromCache(token.id as string, TOKEN_TYPES.ACCESS_TOKEN)
       const refreshTokenExpiry = await getTokenExpiryFromCache(token.id as string, TOKEN_TYPES.REFRESH_TOKEN)
 
-      token.refresh_token_expiry = refreshTokenExpiry
-      // METHOD TO SET STATE AND REFRESH EXPIRY TIME ON TOKEN
+      // Update refresh token expiry
+      updateTokenWithRefreshTokenExpiry(token, refreshTokenExpiry)
 
-      const ONE_MINUTE = 60 * 1000
-      const ZERO_MINUTES = 0 * ONE_MINUTE;
-      const TEN_MINUTES = 10 * ONE_MINUTE;
-
-      if (refreshTokenExpiry <= ZERO_MINUTES)
-        token.refresh_token_state = TOKEN_STATE.EXPIRED
-      else if (refreshTokenExpiry < TEN_MINUTES)
-        token.refresh_token_state = TOKEN_STATE.LESS_THAN_TEN_MINUTES
-      else 
-        token.refresh_token_state = TOKEN_STATE.MORE_THAN_TEN_MINUTES
-
-      // refresh access token if necessary
-      // if access token not expiring in a minute, do nothing
+      // Refresh access token if necessary
       if (accessTokenExpiry > ONE_MINUTE)
          return token;
 
@@ -72,9 +47,8 @@ export const authOptions: NextAuthOptions = {
         try {
           refreshAccessToken(token.id)
         } catch (e) {
-          // Assume any error means token is expired, re-trigger sign in?
-          token.refresh_token_state = TOKEN_STATE.EXPIRED
-          token.refresh_token_expiry = ZERO_MINUTES
+          // Assume any refreshing error means refresh token is expired
+          updateTokenWithRefreshTokenExpiry(token, ZERO_MINUTES)
         }
       return token
     },
@@ -88,6 +62,52 @@ export const authOptions: NextAuthOptions = {
     }
   }
 };
+
+const updateTokenWithRefreshTokenExpiry = (token: JWT, refreshTokenExpiry: number) => {
+  token.refresh_token_expiry = refreshTokenExpiry
+  token.refresh_token_state = toTokenState(refreshTokenExpiry)
+}
+
+const handleTokensOnSignIn = async (token: JWT, account: Account | null) => {
+    // Assign ID to identify session
+    const sessionId = randomUUID()
+    token.id = sessionId
+
+    // Update refresh token expiry
+    const refreshTokenExpiry = account?.refresh_expires_in ?? 0
+    updateTokenWithRefreshTokenExpiry(token, refreshTokenExpiry)
+
+    // Cache tokens
+    cacheTokens(account, sessionId)
+}
+
+const toTokenState = (tokenExpiry: number) => {
+  if (tokenExpiry <= ZERO_MINUTES)
+    return TOKEN_STATE.EXPIRED
+  else if (tokenExpiry < TEN_MINUTES)
+    return TOKEN_STATE.LESS_THAN_TEN_MINUTES
+  else 
+    return TOKEN_STATE.MORE_THAN_TEN_MINUTES
+}
+
+const cacheTokens = async (account: Account | null, sessionId: string) => {
+  // Extract tokens
+  const access_token: JWTWithExpiry = {
+    token: account?.access_token ?? "",
+    expires_in: (account?.expires_at ?? Date.now()) - Math.floor(Date.now() / 1000),
+    type: TOKEN_TYPES.ACCESS_TOKEN
+  }
+
+  const refresh_token: JWTWithExpiry = {
+    token: account?.refresh_token ?? "",
+    expires_in: account?.refresh_expires_in ?? 0,
+    type: TOKEN_TYPES.REFRESH_TOKEN
+  }
+
+  // Cache tokens
+  await cacheToken(access_token, sessionId)
+  await cacheToken(refresh_token, sessionId)
+} 
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
